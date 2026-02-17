4f7637d32539ac6be851ff242ce61944862aee84
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-02-17 16:08:55 +0200

    fix: home route

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', fn () => redirect('/login'));
+Route::get('/', fn () => redirect('/login'))->name('home');
 
 Route::get('dashboard', function () {
     return Inertia::render('dashboard');
