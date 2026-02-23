8587f1dfac826b1ec33a3ed38262de566c85884c
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-02-23 17:11:44 +0200

    feat: user relationship to recipes

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Recipe extends Model
 {
@@ -16,4 +17,9 @@ class Recipe extends Model
     protected $fillable = [
         'name', 'instructions', 'image_url', 'source_url',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
