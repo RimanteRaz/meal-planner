9e04a4b1772240f6678cb6a56d5d3febab617029
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-02-23 16:31:03 +0200

    feat: recipe model

---
+++ database/migrations/2026_02_23_130602_create_recipes_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->string('name');
+            $table->text('instructions');
+            $table->string('image_url')->nullable();
+            $table->string('source_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
