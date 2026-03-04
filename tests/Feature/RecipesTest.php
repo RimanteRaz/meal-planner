9c05f0e63dd4e8498584dade441825460c4faf60
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-03-04 15:38:16 +0200

    test: check if user sees only their recipes

--- tests/Feature/RecipesTest.php
+++ tests/Feature/RecipesTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Recipe;
 use App\Models\User;
+use Inertia\Testing\AssertableInertia;
 
 test('guests are redirected to the login page', function () {
     $response = $this->get(route('recipes.index'));
@@ -13,4 +15,15 @@ test('authenticated users can visit the dashboard', function () {
 
     $response = $this->get(route('recipes.index'));
     $response->assertOk();
+});
+
+test('user only sees their own recipes', function () {
+    $user1 = User::factory()->has(Recipe::factory(['name' => 'User1 Recipe']))->create();
+    User::factory()->hasRecipes(Recipe::factory(['name' => 'Other User Recipe']))->create();
+    $this->actingAs($user1);
+
+    $response = $this->get(route('recipes.index'));
+    $response->assertOk();
+    $response->assertInertia(fn (AssertableInertia $page) => $page->has('recipes.data', 1));
+    $response->assertInertia(fn (AssertableInertia $page) => $page->where('recipes.data.0.name', 'User1 Recipe'));
 });
