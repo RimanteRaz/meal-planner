e090cc1957aa91397178a8cbfeda33aae6863290
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-03-04 12:38:15 +0200

    feat: paginate recipes

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\RecipeResource;
 use App\Models\Recipe;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -15,7 +14,7 @@ class RecipeController extends Controller
     public function index(Request $request)
     {
         return Inertia::render('recipes/index', [
-            'recipes' => RecipeResource::collection($request->user()->recipes),
+            'recipes' => Recipe::latest()->where('user_id', $request->user()->id)->paginate()->toResourceCollection(),
         ]);
     }
 
