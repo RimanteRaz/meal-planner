4e6ad8540a54f9c080852aea94573bf07993e080
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-03-17 16:05:42 +0200

    fix: according to comments

--- resources/js/pages/recipes/RecipePage.tsx
+++ resources/js/pages/recipes/RecipePage.tsx
@@ -1,3 +1,4 @@
+import { Head } from '@inertiajs/react';
 import { LinkIcon } from 'lucide-react';
 import type { ReactNode } from 'react';
 import { Image } from '@/components/ui/image';
@@ -11,25 +12,35 @@ type RecipeProps = {
 
 const RecipePage = ({ recipe }: RecipeProps) => {
     return (
-        <div className="mx-auto max-w-3xl space-y-4 p-6">
-            <Image
-                src={recipe.image}
-                alt={recipe.name}
-                className="h-72 w-full rounded-lg border object-cover object-center"
-            />
-            {recipe.source && (
-                <div className="flex items-center gap-1 text-muted-foreground">
-                    <LinkIcon size={16} />
-                    <a href={recipe.source}>{recipe.source}</a>
-                </div>
-            )}
-            <h1 className="text-2xl font-bold capitalize">{recipe.name}</h1>
+        <>
+            <Head title={recipe.name} />
 
-            <div className="space-y-2">
-                <h3 className="text-xl font-bold">Instructions</h3>
-                <div>{recipe.instructions}</div>
+            <div className="mx-auto max-w-3xl space-y-4 p-6">
+                <Image
+                    src={recipe.image}
+                    alt={recipe.name}
+                    className="h-72 w-full rounded-lg border object-cover object-center"
+                />
+                {recipe.source && (
+                    <div className="flex items-center gap-1 text-muted-foreground">
+                        <LinkIcon size={16} />
+                        <a
+                            href={recipe.source}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            {recipe.source}
+                        </a>
+                    </div>
+                )}
+                <h1 className="text-2xl font-bold capitalize">{recipe.name}</h1>
+
+                <div className="space-y-2">
+                    <h3 className="text-xl font-bold">Instructions</h3>
+                    <div>{recipe.instructions}</div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
