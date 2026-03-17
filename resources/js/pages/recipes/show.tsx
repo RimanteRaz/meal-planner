05515aa2acb117de2eb3044522ba3af616c3d4c9
Rimantė Ražinskaitė <rimante@rimanteraz.com>
2026-03-17 13:56:39 +0200

    feat: show recipe page

---
+++ resources/js/pages/recipes/show.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react';
+import { Image } from '@/components/ui/image';
+import AppLayout from '@/layouts/app-layout';
+import recipes from '@/routes/recipes';
+import type { BreadcrumbItem, RecipeData } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Recipes',
+        href: recipes.index.url(),
+    },
+];
+
+type RecipeProps = {
+    recipe: RecipeData;
+};
+
+const ShowRecipe = ({ recipe }: RecipeProps) => {
+    return (
+        <div className="mx-auto max-w-3xl space-y-4 p-6">
+            <Image
+                src={recipe.image}
+                alt={recipe.name}
+                className="h-72 w-full rounded-lg border object-cover object-center"
+            />
+            <h1 className="text-2xl font-bold capitalize">{recipe.name}</h1>
+            <div className="space-y-2">
+                <h3 className="text-xl font-bold">Instructions</h3>
+                <div>{recipe.instructions}</div>
+            </div>
+        </div>
+    );
+};
+
+ShowRecipe.layout = (page: ReactNode) => (
+    <AppLayout breadcrumbs={breadcrumbs}>{page}</AppLayout>
+);
+
+export default ShowRecipe;
